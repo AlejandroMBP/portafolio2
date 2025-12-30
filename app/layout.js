91a2a0aa16cec2c78ce4b3aeea8b6717b603fa2c
AlejandroMBP <marcosberriosal@gmail.com>
2025-12-30 11:37:10 -0400

    agregado de etiqueta en head

--- app/layout.js
+++ app/layout.js
@@ -51,6 +51,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7157260105016499"
+     crossorigin="anonymous"></script>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${outfit.variable} ${firaCode.variable} antialiased`}>
